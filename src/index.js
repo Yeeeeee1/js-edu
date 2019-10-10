e5d7fd74e61d6b71be558f170d8823d2a3bd105a
Yeeeeee1 <52548740+Yeeeeee1@users.noreply.github.com>
2019-10-10 23:35:09 +0300

    Start project

--- src/index.js
+++ src/index.js
@@ -5,10 +5,17 @@
  * @returns number of weeks needed for finish education
  */
 module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
+  focus = 'family', 
+  knowsProgramming = true,
+  config = {family: 4}
+  ) {
+    if (knowsProgramming == true) {
+      let hours = 800;
+      return Math.ceil(hours / config[focus]);
+    }
+    else {
+      let hours = 1300;
+      return Math.ceil(hours / config[focus]);
+    }
+};
   
